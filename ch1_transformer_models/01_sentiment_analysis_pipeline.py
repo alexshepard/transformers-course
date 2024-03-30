06b5da554b04e8455f35bf91248a2669eb213927
Alex Shepard <alex@meatfreezer.com>
2024-03-30 13:14:57 -0700

    sentiment analysis on several sentences

--- ch1_transformer_models/01_sentiment_analysis_pipeline.py
+++ ch1_transformer_models/01_sentiment_analysis_pipeline.py
@@ -1,5 +1,15 @@
 from transformers import pipeline
 
-classifier = pipeline("sentiment-analysis")
-input = "I've been waiting for a HuggingFace course my whole life."
-print(classifier(input))
+def main():
+    classifier = pipeline("sentiment-analysis")
+    input = "I've been waiting for a HuggingFace course my whole life."
+    print(input, classifier(input))
+
+    inputs = [
+        "I've been waiting for a HuggingFace course my whole life.",
+        "I hate this so much!"
+    ]
+    print(list(zip(inputs, classifier(inputs))))
+
+if __name__ == "__main__":
+    main()
