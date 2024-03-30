12d86a97d6b4e93747cf1cf6c0cd3fcee202d3d9
Alex Shepard <alex@meatfreezer.com>
2024-03-30 14:11:32 -0700

    use mistral here

--- ch1_transformer_models/04_using_huggingface_models.py
+++ ch1_transformer_models/04_using_huggingface_models.py
@@ -9,8 +9,8 @@ def main():
     print("distilgpt2", generator(input, num_return_sequences=2, max_length=30))
 
     # very large model
-    generator = pipeline("text-generation", model="georgesung/llama2_7b_chat_uncensored")
-    print("llama", generator(input, max_length=30))
+    generator = pipeline("text-generation", model="mistralai/Mistral-7B-v0.1")
+    print("mistral", generator(input, max_length=30))
 
 
 if __name__ == "__main__":
