6c04b63b758cb74e1461934930a97588c007d6b9
Alex Shepard <alex@meatfreezer.com>
2024-03-30 13:26:26 -0700

    zero shot classification

---
+++ ch1_transformer_models/02_zero_shot_classification.py
@@ -0,0 +1,15 @@
+from transformers import pipeline
+
+def main():
+    classifier = pipeline("zero-shot-classification")
+    input = "This is a course about the Transfomers library"
+    candidate_labels = ["education", "politics", "business"]
+    print(classifier(input, candidate_labels))
+
+    input = "That's great, it starts with an earthquake / Birds and snakes, and aeroplanes / And Lenny Bruce is not afraid"
+    candidate_labels = ["education", "politics", "business", "lyrics"]
+    print(classifier(input, candidate_labels))
+
+
+if __name__ == "__main__":
+    main()
